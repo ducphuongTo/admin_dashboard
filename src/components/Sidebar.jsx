971014fdd2f1db66609154f6d147351607e365d9
phuong.toduc <phuong.toduc@nastechglobal.com>
2024-04-14 19:19:26 +0700

    Build navbar and add context api provider

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,22 +8,21 @@ import { links } from '../data/dummy';
 
 
 const Sidebar = () => {
-  
-  const activeMenu = true;
+  const { currentColor, activeMenu, setActiveMenu, screenSize } = useStateContext();
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
-  // const handleCloseSideBar = () => {
-  //   if (activeMenu !== undefined && screenSize <= 900) {
-  //     setActiveMenu(false)
-  //   }
-  // }
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
         {
           activeMenu && (
             <>
               <div className='flex justify-between items-center'>
-                <Link to="/" onClick={() => {} } className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+                <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
                   <SiShopware/> <span>Shoppy</span>
                 </Link>
 
@@ -32,7 +31,7 @@ const Sidebar = () => {
                     className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
 
                     type='button'
-                    onClick={() => {}}
+                    onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu) }
                   >
                   <MdOutlineCancel />
                   </button>
@@ -49,8 +48,7 @@ const Sidebar = () => {
                       <NavLink
                         to={`/${link.name}`}
                         key={link.name}
-                        
-                        
+                        onClick={handleCloseSideBar}
                         className={({ isActive }) => (isActive ? activeLink : normalLink)}
                       >
                         {link.icon}
